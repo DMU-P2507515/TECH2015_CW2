8a8073d2d4b8119432cfbdb43274148bd6cbf697
Roy Cheng <roystekii2@gmail.com>
2022-05-01 21:04:58 +0100

    coursework2

--- PHP/contactform.php
+++ PHP/contactform.php
@@ -4,9 +4,9 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "soapfilms01@gmail.com";
-$subject = "SoapFilms Video Submission Form";
+$subject = "SoapFilms contact form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
-header("Location: homepage.html");
+header("Location: about.html");
 ?>
