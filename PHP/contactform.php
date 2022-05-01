98097c4bd2b1be87e6afb3d60dd0e9bb085e82d3
Roy Cheng <roystekii2@gmail.com>
2022-05-01 20:54:09 +0100

    coursework2

---
+++ PHP/contactform.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "soapfilms01@gmail.com";
+$subject = "SoapFilms Video Submission Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+header("Location: homepage.html");
+?>
